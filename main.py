5b441065391769509feee52d11fcdab3491d050b
jhackisneros <oliveracisneros1@gmail.com>
2025-04-30 11:37:29 +0200

    cambios

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from interfaz.ventana_principal import VentanaPrincipal
 from base_datos.csv_manager import CSVManager
 
 def run_app():
-    csv_manager = CSVManager('libros.csv', 'usuarios.csv', 'prestamos.csv')
+    csv_manager = CSVManager("libros.csv","usuarios.csv","prestamos.csv")
     libros = csv_manager.cargar_libros()
     usuarios = csv_manager.cargar_usuarios()
 
